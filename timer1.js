74adaa40ea3be1d73e8d70cf5653891c1b38ea34
hildakh <h_khajehamiri@yahoo.com>
2019-10-23 02:08:59 +0000

    finally finished the multiple onput timer using recursion

--- timer1.js
+++ timer1.js
@@ -1,12 +1,24 @@
 var args = process.argv.slice(2);
-
-const timer = function(duration) {
-  if(duration <= 0 || isNaN(duration)) {
+// console.log(args);
+const timer = function(inputArray, index = 0) {
+  // console.log(inputArray);
+  if(inputArray.length < 1 || inputArray.length === index) {
     return;
   }
-  setTimeout(() => {
-    console.log('It\'s time!');
-  }, duration);
+  if(typeof(Number(inputArray[index])) === 'number') {
+    const duration = inputArray[index];
+    
+    setTimeout(() => {
+        console.log(`It's time!`);
+      }, duration * 1000);
+
+    timer(inputArray, index+1);
+  }
+
+
 }
 
-timer(args);
+
+timer(args);  
+
+///needs completion
